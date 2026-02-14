3510831f1b3909578968a69fecb3f5238534b28f
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-02-14 13:15:39 +0530

    add some functionality to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import './App.css'
+import {Header, Footer} from "./components/index"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authsService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+
 
 
 function App() {
 
-  return (
-    <>
-      <h1>Blog with Appwrite</h1>
-    </>
-  )
+  const [loading , setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authsService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch({userData})
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+          <Header />
+          <main>
+            <h1>Test</h1>
+          </main>
+          <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
