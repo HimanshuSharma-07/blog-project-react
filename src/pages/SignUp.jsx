238cdc5000bbebeb9018b89ed932b30d9cad3e39
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-02-19 01:19:14 +0530

    Create Signup page

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup as SignupCompnent } from '../components'
+function SignUp() {
+  return (
+    <div className='py-8'>
+        <SignupCompnent />
+    </div>
+  )
+}
+
+export default SignUp
